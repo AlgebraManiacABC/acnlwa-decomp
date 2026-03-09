779ff0f1afc7e15053e91970f5210ef1e960d88f
AlgebraManiacABC <dunnluke01@gmail.com>
2026-03-09 05:14:38 -0500

    Decomp FUN_00583778 (byte.c)

--- versions/CTR-P-EAAE/src/code.bin/ghidra/byte.c
+++ versions/CTR-P-EAAE/src/code.bin/ghidra/byte.c
@@ -1,15 +1,10 @@
-/**
- * byte.c
- * Source line: 829354
- * Body lines: 3
- */
 #include "../../../include/types.h"
 
-byte (*) [16] FUN_00583778(void)
+extern unsigned long long BYTE_ARRAY_ARRAY_0083b146[][16];
+extern ranqd2_ranged_int_from_seed_2(int);
 
+unsigned long long (*FUN_00583778(void))[16]
 {
-  int ran0_4;
-  
-  ran0_4 = ranqd2_ranged_int_from_seed_2(4);
-  return BYTE_ARRAY_ARRAY_0083b146 + ran0_4;
+    int ran0_4 = ranqd2_ranged_int_from_seed_2(4);
+    return BYTE_ARRAY_ARRAY_0083b146 + ran0_4;
 }
